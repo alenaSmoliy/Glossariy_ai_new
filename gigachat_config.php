28ac2c0d3997ae8235a47e14acf4aa244fb9a750
alenaSmoliy <smoliy.alena@yandex.ru>
2026-04-03 13:42:04 +0700

    Create gigachat_config.php

---
+++ gigachat_config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Конфигурация GigaChat API
+ * 
+ * @package    local_glossary_ai
+ * @author     Смолий Алена
+ * @copyright  2026 Алтайский государственный университет
+ */
+
+return [
+    'client_id' => '25990d6f-112d-4bf5-b097-dcaac4b6b226',
+    'client_secret' => '74fb5814-2c41-4714-a9a1-d4901d579a76',
+    'scope' => 'GIGACHAT_API_B2B',
+    'model' => 'GigaChat-2-Pro',
+    'temperature' => 0.7,
+    'timeout' => 120
+];
