523c5b47ccba0822ee1570fd0a57a142352cf64f
alenaSmoliy <smoliy.alena@yandex.ru>
2026-04-03 13:46:23 +0700

    Create local_glossary_ai.php

---
+++ lang/en/local_glossary_ai.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * English language pack for "AI Glossary" plugin
+ *
+ * @package    local_glossary_ai
+ * @author     Alena Smoliy
+ * @copyright  2026 Altai State University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'AI Glossary';
+$string['pluginname_desc'] = 'Automatic glossary terms generation using GigaChat AI';
+
+$string['builder'] = 'AI Glossary';
+$string['generate_new'] = 'Generate New Terms';
+$string['edit_terms'] = 'Edit Terms';
+
+$string['select_glossary'] = 'Select Glossary';
+$string['topic'] = 'Topic';
+$string['topic_help'] = 'Enter the topic for glossary terms generation';
+$string['terms_count'] = 'Number of Terms';
+$string['terms_count_help'] = 'Select number of terms (10, 25 or 50)';
+$string['language'] = 'Language';
+$string['source_file'] = 'Source File (optional)';
+$string['source_file_help'] = 'Upload .txt or .docx file for context';
+$string['custom_prompt'] = 'Additional Instructions';
+$string['custom_prompt_help'] = 'E.g., "Add usage examples"';
+$string['generate_btn'] = 'Generate Terms';
+
+$string['error_no_glossary'] = 'No glossaries found in this course';
+$string['error_generation'] = 'Error generating terms';
+$string['error_api_not_configured'] = 'GigaChat API is not configured';
+
+$string['instruction_link'] = 'Instruction Link';
+$string['instruction_link_desc'] = 'URL of the plugin usage instructions page';
+
+$string['glossary_ai:use'] = 'Use AI Glossary plugin';
+$string['glossary_ai:manage'] = 'Manage AI Glossary plugin settings';
