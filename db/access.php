bc9149610efe29ac136dd129dd701734ee9eddbe
alenaSmoliy <smoliy.alena@yandex.ru>
2026-04-03 13:50:34 +0700

    Create access.php

---
+++ db/access.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Права доступа для плагина "Глоссарий ИИ"
+ *
+ * @package    local_glossary_ai
+ * @author     Смолий Алена
+ * @copyright  2026 Алтайский государственный университет
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    
+    'local/glossary_ai:use' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ],
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ],
+    
+    'local/glossary_ai:manage' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'manager' => CAP_ALLOW
+        ],
+        'clonepermissionsfrom' => 'moodle/site:config'
+    ]
+];
